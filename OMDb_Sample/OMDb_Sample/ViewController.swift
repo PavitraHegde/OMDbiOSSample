3eb38a246b7543537addeccd52915d6f8c43b759
pavitra <pavitra.hegde@cartus.com>
2020-03-20 15:00:37 +0530

    Initial commit

---
+++ OMDb_Sample/OMDb_Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OMDb_Sample
+//
+//  Created by Pavitra Hegde on 20/03/20.
+//  Copyright © 2020 Pavitra Hegde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
